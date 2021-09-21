562af9e605431759e145de117d40db271da7fddd
tanjunxin <tanjunxin0819@163.com>
2021-09-21 22:20:29 +0800

    已完成react-redux的应用

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import React, { useEffect, useState } from 'react'
-// import TestProps from './demo/props/index'
-// import TestHook from './demo/hook/index'
-// import TestHoc from './demo/hoc/index'
-import TestRedux from './demo/redux/index'
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+import { connect } from 'react-redux'
+// import { increment, decrement } from './demo/react-redux/actions/counter'
+import * as counterActions from './demo/react-redux/actions/counter'
+import * as authActions from './demo/react-redux/actions/auth'
+import { bindActionCreators } from 'redux'
 
-// 引入redux
-import { createStore } from 'redux'
-import rootReducer from './demo/redux/reducers/index'
-
-// 创建仓库(第一个参数是reducer{state的处理函数}, store的数据全部来源于reducer, reducer产生应用的状态)
-const store = createStore(rootReducer)
-
-export default function App() {
-  const [unscribe, SetUnscribe] = useState(store.getState())
-  // 状态改变但是不会马上渲染到视图中, 要通过componentDidUpdate来重新渲染到视图中
-  useEffect(() => {
-    let unscribe = store.subscribe(() => {
-      console.log("store.subscribe监听状态改变的情况", store.getState())
-      SetUnscribe(store.getState())
-    })
-    return () => {
-      // 一定要取消订阅, 否则会重复执行
-      unscribe()
-    }
-  })
-  return (
-    // <TestProps />
-    // <TestHook />
-    // <TestHoc />
-    <TestRedux
-      onIncrement={() => { store.dispatch({ type: "INCREMENT", num: 10 }) }}
-      onDecrement={() => { store.dispatch({ type: "DECREMENT", num: 5 }) }}
-      value={unscribe}
-    />
-  )
+class App extends Component {
+  render() {
+    console.log(this.props)
+    // const { increment, decrement } = this.props
+    return (
+      <Fragment>
+        <h2>{this.props.counter}</h2>
+        {/* 使用dispatch操作 */}
+        {/* <button onClick={() => (increment())}>increment</button>
+        <button onClick={() => (decrement())}>decrement</button> */}
+        {/* 使用bindActionCreators操作*/}
+        <button onClick={() => this.props.counterActions.increment(10)}>increment</button>
+        <button onClick={() => this.props.counterActions.decrement(5)}>decrement</button>
+        <h2>---------------------------</h2>
+        <h2>{this.props.auth}</h2>
+        <button onClick={() => this.props.authActions.addUser("tjx")}>adduser</button>
+      </Fragment>
+    )
+  }
 }
+
+// mapStateToProps是用来渲染数据的
+
+const mapStateToProps = (state) => {
+  return {
+    // 模块化后, state就是一个对象
+    counter: state.counter,
+    auth: state.auth
+  }
+}
+
+// mapDispatchToProps是用来操作数据的
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => {
+//       dispatch(increment(10))
+//     },
+//     decrement: () => {
+//       dispatch(decrement(5))
+//     }
+//   }
+// }
+
+// 使用bindActionCreators来优化mapDispatchToProps操作
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    counterActions: bindActionCreators(counterActions, dispatch),
+    authActions: bindActionCreators(authActions, dispatch)
+  }
+}
+
+// 当使用bindActionCreators来操作redux时, 需要引入react-redux提供的connect来链接redux和组件之间, connect(渲染函数, 操作数据函数)(组件名)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
